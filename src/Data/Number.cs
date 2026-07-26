2e5bb5f8384b8ce31ab6cd4e0b2824160ff41bf5
0x1 <kevin.francart3@gmail.com>
2026-07-26 14:40:15 +0200

    feat: add C# FFI implementations

---
+++ src/Data/Number.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Data.Number;
+
+public static class FFI {
+    public static object IsNaN(object arg1) => throw new NotImplementedException("Not implemented: IsNaN");
+    public static object IsFinite(object arg1) => throw new NotImplementedException("Not implemented: IsFinite");
+    public static object FromStringImpl(object arg1, object arg2) => throw new NotImplementedException("Not implemented: FromStringImpl");
+    public static object Abs(object arg1) => throw new NotImplementedException("Not implemented: Abs");
+    public static object Acos(object arg1) => throw new NotImplementedException("Not implemented: Acos");
+    public static object Asin(object arg1) => throw new NotImplementedException("Not implemented: Asin");
+    public static object Atan(object arg1) => throw new NotImplementedException("Not implemented: Atan");
+    public static object Atan2(object arg1, object arg2) => throw new NotImplementedException("Not implemented: Atan2");
+    public static object Ceil(object arg1) => throw new NotImplementedException("Not implemented: Ceil");
+    public static object Cos(object arg1) => throw new NotImplementedException("Not implemented: Cos");
+    public static object Exp(object arg1) => throw new NotImplementedException("Not implemented: Exp");
+    public static object Floor(object arg1) => throw new NotImplementedException("Not implemented: Floor");
+    public static object Log(object arg1) => throw new NotImplementedException("Not implemented: Log");
+    public static object Max(object arg1, object arg2) => throw new NotImplementedException("Not implemented: Max");
+    public static object Min(object arg1, object arg2) => throw new NotImplementedException("Not implemented: Min");
+    public static object Pow(object arg1, object arg2) => throw new NotImplementedException("Not implemented: Pow");
+    public static object Remainder(object arg1, object arg2) => throw new NotImplementedException("Not implemented: Remainder");
+    public static object Round(object arg1) => throw new NotImplementedException("Not implemented: Round");
+    public static object Sign(object arg1) => throw new NotImplementedException("Not implemented: Sign");
+    public static object Sin(object arg1) => throw new NotImplementedException("Not implemented: Sin");
+    public static object Sqrt(object arg1) => throw new NotImplementedException("Not implemented: Sqrt");
+    public static object Tan(object arg1) => throw new NotImplementedException("Not implemented: Tan");
+    public static object Trunc(object arg1) => throw new NotImplementedException("Not implemented: Trunc");
+}
---
+++ src/Data/Number/Format.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Data.Number.Format;
+
+public static class FFI {
+    public static object ToExponentialNative(object arg1, object arg2) => throw new NotImplementedException("Not implemented: ToExponentialNative");
+    public static object ToFixedNative(object arg1, object arg2) => throw new NotImplementedException("Not implemented: ToFixedNative");
+    public static object ToPrecisionNative(object arg1, object arg2) => throw new NotImplementedException("Not implemented: ToPrecisionNative");
+    public static object ToString(object arg1) => throw new NotImplementedException("Not implemented: ToString");
+}
